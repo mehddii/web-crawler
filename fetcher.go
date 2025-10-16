ce0cac02f2f792bab33db1573667360a3456e16d
mehdii <mehdi7bs@gmail.com>
2025-10-16 23:49:16 +0100

    feat: Implement a simple fetcher

--- fetcher.go
+++ fetcher.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"io"
+	"net/http"
+)
+
 type Fetcher interface {
 	Fetch(uri string) ([]byte, error)
+}
+
+type SimpleFetcher struct{}
+
+func (fetcher *SimpleFetcher) Fetch(uri string) ([]byte, error) {
+	resp, err := http.Get(uri)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	return io.ReadAll(resp.Body)
 }
