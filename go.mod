d10ab72b6978290912687a245df73ea5277b3bd5
mehdii <mehdi7bs@gmail.com>
2025-10-18 23:09:21 +0100

    feat: Implement text and link extraction using net/html

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mehddii/web-crawler
 
 go 1.24.7
+
+require golang.org/x/net v0.46.0
--- parser.go
+++ parser.go
@@ -1,5 +1,72 @@
 package main
 
+import (
+	"bytes"
+	"strings"
+
+	stdhtml "golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+)
+
 type Parser interface {
 	Parse(html []byte) (text string, links []string, err error)
+}
+
+type HTMLParser struct{}
+
+func (parser *HTMLParser) Parse(html []byte) (text string, links []string, err error) {
+	r := bytes.NewReader(html)
+	doc, err := stdhtml.Parse(r)
+	if err != nil {
+		return "", nil, err
+	}
+
+	links = make([]string, 0)
+	for node := range doc.Descendants() {
+		// Extracts http/https links from <a> tags.
+		if node.Type == stdhtml.ElementNode && node.DataAtom == atom.A {
+			for _, attr := range node.Attr {
+				if attr.Key == "href" && strings.HasPrefix(attr.Val, "http") {
+					links = append(links, attr.Val)
+				}
+			}
+		}
+	}
+
+	return extractText(doc), links, nil
+}
+
+// Traverses recursively the html tree and extracts text.
+// All the <script>, <style> and <iframe> nodes are skipped
+// to avoid noise.
+func extractText(node *stdhtml.Node) string {
+	var (
+		builder strings.Builder
+		f       func(node *stdhtml.Node)
+	)
+
+	f = func(node *stdhtml.Node) {
+		if node.Type == stdhtml.ElementNode &&
+			(node.Data == "script" || node.Data == "style" || node.Data == "iframe") {
+			return
+		}
+
+		if node.Type == stdhtml.TextNode && !strings.HasPrefix(node.Data, "<iframe") {
+			data := strings.TrimSpace(node.Data)
+			if data != "" {
+				_, err := builder.WriteString(data)
+				builder.WriteString(" ")
+				if err != nil {
+					return
+				}
+			}
+		}
+
+		for n := node.FirstChild; n != nil; n = n.NextSibling {
+			f(n)
+		}
+	}
+	f(node)
+
+	return strings.TrimSpace(builder.String())
 }
